cd6361298d9b006fffa0ec64011b735ecc455d71
Mats Gefvert <mats@gefvert.org>
2016-12-10 18:45:00 -0500

    Now allows adjusting and scaling, sending output to stdout by default

--- AdjustSRT/Program.cs
+++ AdjustSRT/Program.cs
@@ -1,133 +1,57 @@
 ﻿using System;
-using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Text.RegularExpressions;
+using CommonNetTools;
 
 namespace AdjustSRT
 {
     class Program
     {
-        private readonly static Regex Timecode = new Regex(@"(?<from>\d\d:\d\d:\d\d,\d+)\s*-->\s*(?<to>\d\d:\d\d:\d\d,\d+)");
-        private readonly static string TimecodeFormat = @"hh\:mm\:ss\,fff";
-
-        static int Main(string[] args)
+        static int Main()
         {
             try
             {
-                if (args.Length == 0)
-                    return DisplayHelp();
+                CommandLine.DisplayHelpOnEmpty = true;
+                var args = CommandLine.Parse<SrtParameters>();
 
-                if (args.Length != 2)
-                    throw new Exception("Invalid parameters.");
+                if (string.IsNullOrEmpty(args.InFile))
+                    throw new Exception("Missing input file");
 
-                var srtFile = args[0];
-                if (!File.Exists(srtFile))
-                    throw new FileNotFoundException("Unable to locate " + srtFile, srtFile);
+                if (!File.Exists(args.InFile))
+                    throw new Exception($"Input file {args.InFile} not found");
 
-                var offset = !args[1].Contains(":") 
-                    ? TimeSpan.FromSeconds(double.Parse(args[1], CultureInfo.InvariantCulture)) 
-                    : TimeSpan.Parse(args[1], CultureInfo.InvariantCulture);
-
-                return ProcessFile(srtFile, offset);
+                SrtProcessor.Process(args);
+                return 0;
+            }
+            catch (CommandLineDisplayHelpException ex)
+            {
+                DisplayHelp(ex.Message);
+                return 1;
             }
             catch (Exception ex)
             {
-                using (new FgColor(ConsoleColor.Red))
-                    Console.WriteLine((ex.GetType() != typeof(Exception) ? ex.GetType().Name + ": " : "") + ex.Message);
+                using (new SetConsoleColor(ConsoleColor.Red))
+                    Console.Error.WriteLine((ex.GetType() != typeof(Exception) ? ex.GetType().Name + ": " : "") + ex.Message);
+
                 return 1;
             }
         }
 
-        private static int ProcessFile(string filename, TimeSpan offset)
+        private static void DisplayHelp(string message)
         {
-            Console.WriteLine($"Adjusting {filename} by {offset.ToString().TrimEnd('0')}");
+            Console.Error.WriteLine("AdjustSRT <parameters>");
+            Console.Error.WriteLine(message);
+            Console.Error.WriteLine(@"
+Examples:
 
-            var lines = File.ReadAllLines(filename).ToList();
-            for (int i = 0; i < lines.Count; i++)
-            {
-                var match = Timecode.Match(lines[i]);
-                if (!match.Success)
-                    continue;
+AdjustSRT -i infile.srt --sub 20
+    Adjust SRT file with -20 secs.
 
-                var from = ParseTime(match.Groups["from"].Value, i+1);
-                var to = ParseTime(match.Groups["to"].Value, i+1);
-                if (from == null || to == null)
-                    continue;
+AdjustSRT -i infile.srt --add 00:00:05.400
+    Adjust SRT file with +5.4 secs.
 
-                var from2 = from.Value.Add(offset);
-                var to2 = to.Value.Add(offset);
-                if (from2.Ticks < 0 || to2.Ticks < 0)
-                    Console.WriteLine("Warning: Timecode is negative on line " + (i+1));
-
-                lines[i] = 
-                    (from2.Ticks < 0 ? "-" : "") + from2.ToString(TimecodeFormat) +
-                    " --> " +
-                    (to2.Ticks < 0 ? "-" : "") + to2.ToString(TimecodeFormat);
-            }
-
-            var offsetText = offset.TotalSeconds.ToString("+0.000;-0.000", CultureInfo.InvariantCulture)
-                .TrimEnd('0')
-                .TrimEnd('.');
-
-            var newfilename = Path.GetFileNameWithoutExtension(filename) + offsetText + Path.GetExtension(filename);
-            File.WriteAllLines(newfilename, lines);
-            Console.WriteLine("Saved new SRT file as " + newfilename);
-            return 0;
-        }
-
-        private static TimeSpan? ParseTime(string text, int line)
-        {
-            if (string.IsNullOrEmpty(text))
-            {
-                Console.WriteLine("Missing timecode on line " + line);
-                return null;
-            }
-
-            TimeSpan ts;
-            if (TimeSpan.TryParseExact(text, TimecodeFormat, CultureInfo.InvariantCulture, out ts))
-                return ts;
-
-            if (TimeSpan.TryParse(text, out ts))
-            {
-                Console.WriteLine($"Inexact timecode match on line {line}: {text}");
-                return ts;
-            }
-
-            Console.WriteLine($"Invalid timecode on line {line}: {text}");
-            return null;
-        }
-
-        private static int DisplayHelp()
-        {
-            Console.WriteLine(@"AdjustSRT <filename> <timestamp>
-
-Filename is the SRT file you want to adjust.
-Timestamp is either number of seconds, or a valid timestamp in 'hh:mm:ss.fff'.
-
-The new file will be saved as 'filename-timestamp.srt'.
-
-Example:
-  AdjustSRT 'Forbidden Planet.srt' -20           # Adjust with -20 secs
-  AdjustSRT 'Forbidden Planet.srt' 00:00:05.400  # Adjust with +5.4 secs
-".Replace('\'', '\"'));
-            return 1;
-        }
-    }
-
-    public class FgColor : IDisposable
-    {
-        private readonly ConsoleColor _savedColor;
-
-        public FgColor(ConsoleColor fg)
-        {
-            _savedColor = Console.ForegroundColor;
-            Console.ForegroundColor = fg;
-        }
-
-        public void Dispose()
-        {
-            Console.ForegroundColor = _savedColor;
+AdjustSRT -i infile.srt --scale 1.1 -o out.srt
+    Scale infile.srt with 10% and write to out.srt.
+");
         }
     }
 }
---
+++ AdjustSRT/SrtProcessor.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AdjustSRT
+{
+    public static class SrtProcessor
+    {
+        private readonly static Regex Timecode = new Regex(@"(?<from>\d\d:\d\d:\d\d,\d+)\s*-->\s*(?<to>\d\d:\d\d:\d\d,\d+)");
+        private readonly static string TimecodeFormat = @"hh\:mm\:ss\,fff";
+
+        public static void Process(SrtParameters parameters)
+        {
+            var scale = parameters.ScaleValue ?? 1;
+            var adjust = parameters.AddValue.Subtract(parameters.SubValue);
+
+            Console.Error.WriteLine("Adjusting {0} : scale={1} offset={2}",
+                parameters.InFile, scale.ToString(CultureInfo.InvariantCulture), adjust);
+
+            var result = string.IsNullOrEmpty(parameters.OutFile) 
+                ? Console.Out 
+                : new StreamWriter(parameters.OutFile, false, Encoding.UTF8);
+            try
+            {
+                using (var reader = new StreamReader(parameters.InFile, Encoding.UTF8))
+                {
+                    string line;
+                    int i = 1;
+                    TimeSpan? first = null;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        var match = Timecode.Match(line);
+                        if (!match.Success)
+                        {
+                            // Regular line
+                            result.WriteLine(line);
+                            continue;
+                        }
+
+                        var from = ParseTime(match.Groups["from"].Value, i);
+                        var to = ParseTime(match.Groups["to"].Value, i);
+                        if (from == null || to == null)
+                        {
+                            // Error in processing... weird.
+                            result.WriteLine(line);
+                            continue;
+                        }
+
+
+                        if (first == null)
+                            first = from.Value;
+
+                        var from2 = ProcessTime(from.Value, first.Value, scale, adjust);
+                        var to2 = ProcessTime(to.Value, first.Value, scale, adjust);
+                        if (from2.Ticks < 0 || to2.Ticks < 0)
+                            Console.WriteLine("Warning: Timecode is negative on line " + (i + 1));
+
+                        result.WriteLine((from2.Ticks < 0 ? "-" : "") + from2.ToString(TimecodeFormat) +
+                            " --> " +
+                            (to2.Ticks < 0 ? "-" : "") + to2.ToString(TimecodeFormat));
+
+                        i++;
+                    }
+                }
+            }
+            finally
+            {
+                if (!string.IsNullOrEmpty(parameters.OutFile))
+                    result.Dispose();
+            }
+        }
+
+        private static TimeSpan ProcessTime(TimeSpan value, TimeSpan first, double scale, TimeSpan adjust)
+        {
+            return TimeSpan.FromSeconds((value - first).TotalSeconds * scale).Add(first).Add(adjust);
+        }
+
+        private static TimeSpan? ParseTime(string text, int line)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                Console.WriteLine("Missing timecode on line " + line);
+                return null;
+            }
+
+            TimeSpan ts;
+            if (TimeSpan.TryParseExact(text, TimecodeFormat, CultureInfo.InvariantCulture, out ts))
+                return ts;
+
+            if (TimeSpan.TryParse(text, out ts))
+            {
+                Console.WriteLine($"Inexact timecode match on line {line}: {text}");
+                return ts;
+            }
+
+            Console.WriteLine($"Invalid timecode on line {line}: {text}");
+            return null;
+        }
+    }
+}
